0436c3a1505d047fc1ef90b2d060205057be8344
Arshia <arshiashirvani.f1385@gmail.com>
2024-09-04 16:34:39 +0330

    wrote about how to user login and redirect to personal information
    Connect login to profile page

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    # path('login',views.UserLoginView.as_view(),name='login-view'),
+    path('login',views.login_view,name='login-view'),
+    path('profile',views.user_profile,name='profile-view'),
     # path('login',views.UserRegisterView.as_view(),name='login-view'),
     # path('login',views.UserCompleteRegisterView.as_view(),name='login-view'),
     # path('login',views.UserForgetPasswordView.as_view(),name='login-view'),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,1 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib.auth.views import LoginView
+from django.views.generic import DetailView
+from django.contrib.auth import authenticate,login
+from django.urls import reverse_lazy,reverse
+from .models import Profile,User
+
+def login_view(request):
+    if not request.user.is_authenticated:
+          if request.method=='POST':
+               username=request.POST['username']
+               password=request.POST['password']
+               user = authenticate(request,username=username, password=password)
+               if user is not None:
+                    login(request,user)
+                    user_id = user.id
+                    # return HttpResponseRedirect(reverse('accounts:profile-view', kwargs={"pk": user.national_code}))
+                    return HttpResponseRedirect(reverse('accounts:profile-view'))
+                    
+          return render(request,'accounts/login.html')
+    else:
+        return redirect('/')
+        
+    
+def user_profile(request):
+    if request.user.is_authenticated:
+        user = get_object_or_404(Profile,pk=request.user.id)
+        context = {'user':user}
+        return render(request,'accounts/profile.html',context)
