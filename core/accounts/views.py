1993a90366fc1c9d9c74d5303d25df9db3659863
Arshia <arshiashirvani.f1385@gmail.com>
2024-10-07 12:37:28 +0330

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.urls import reverse_lazy,reverse
 from django.views.generic.edit import CreateView,FormView
 
-from .models import Profile,User,UserManager
+from .models import Profile,User
 from .forms import UserRegisterForm
 
 def login_view(request):
